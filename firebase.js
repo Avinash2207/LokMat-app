3b62c5bd1cad9aeb55a92708784298daa6543cd5
Avinash2207 <jha.avi2702@gmail.com>
2025-12-03 00:31:20 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,21 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDPQBoOhNWSqUMEcawIF9U7zExUYe_oe1g",
   authDomain: "lokmat-app.firebaseapp.com",
   projectId: "lokmat-app",
   storageBucket: "lokmat-app.firebasestorage.app",
   messagingSenderId: "253512412538",
-  appId: "1:253512412538:web:25c4448943cf369a5d7ac3"
+  appId: "1:253512412538:web:25c4448943cf369a5d7ac3",
+  measurementId: "G-L94FN8GW2D"
 };
 
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
-export const auth = firebase.auth();
-export const db = firebase.firestore();
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
