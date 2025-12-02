f5249c59c3fe4530bc323ab6e610487362743ad5
Avinash2207 <jha.avi2702@gmail.com>
2025-12-02 23:15:47 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,18 +1,17 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD0d8Z8Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z9Z", // ← we’ll replace this in 2 minutes
-  authDomain: "lokmat-2025.firebaseapp.com",
-  projectId: "lokmat-2025",
-  storageBucket: "lokmat-2025.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyDPQBoOhNWSqUMEcawIF9U7zExUYe_oe1g",
+  authDomain: "lokmat-app.firebaseapp.com",
+  projectId: "lokmat-app",
+  storageBucket: "lokmat-app.firebasestorage.app",
+  messagingSenderId: "253512412538",
+  appId: "1:253512412538:web:25c4448943cf369a5d7ac3",
+  measurementId: "G-L94FN8GW2D"
 };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-export const functions = getFunctions(app);
